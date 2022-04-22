395561e88d9e00268899b0a39eb2ead0677ee933
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-22 10:10:20 -0500

    made letsGetSomeIceCream even prettier

--- js/while.js
+++ js/while.js
@@ -10,14 +10,14 @@
 
     function letsGetSomeIceCream(){
         let inventory = Math.floor(Math.random() * 50) + 50
-
+        console.log('I have ' + inventory + " cones. Let's get to selling");
         do {
             let yummy = Math.floor(Math.random() * 5) + 1
-            console.log('I sold ' + yummy + ' cones, I have ' + (inventory - yummy) + ' cones left, better keep selling.');
             if (yummy > inventory) {
-                console.log('Drat, I only have ' + inventory + " cones left, I can't make that sale.");
-                continue
+                console.log('Drat, the customer wants ' + yummy + ' cones but I only have ' + inventory + " cones left, I can't make that sale.");
+                break
             }
+            console.log('I sold ' + yummy + ' cones, I have ' + (inventory - yummy) + ' cones left, better keep selling.');
             inventory -= yummy;
         } while (inventory > 0);
         console.log('Time to blow this popsicle stand!');
