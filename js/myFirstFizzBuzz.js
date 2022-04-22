9c540ef9cb6729684a6f4c8e702efeada17e5313
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-22 09:20:13 -0500

    FizzBuzz as warmup exercise

---
+++ js/myFirstFizzBuzz.js
@@ -0,0 +1,15 @@
+(function(){
+function fizzBuzz(){
+    for (let count = 1; count <=100 ; count++){
+        switch (true) {
+            case (count % 3 === 0 & count % 5 === 0) : console.log('FizzBuzz');
+            break
+            case (count % 3 === 0) : console.log('Fizz');
+            break
+            case (count % 5 === 0) : console.log('Buzz');
+            break
+            default : console.log(count);
+        }
+    }
+}fizzBuzz();
+})()
--- js/warmups.js
+++ js/warmups.js
@@ -1,2 +1,23 @@
-"use strict"
-console.log("Yay! I completed the warmup!")
+(function() {
+    "use strict"
+    console.log("Yay! I completed the warmup!")
+
+    function fizzBuzz() {
+        for (let count = 1; count <= 100; count++) {
+            switch (true) {
+                case (count % 3 === 0 && count % 5 === 0) :
+                    console.log('FizzBuzz');
+                    break
+                case (count % 3 === 0) :
+                    console.log('Fizz');
+                    break
+                case (count % 5 === 0) :
+                    console.log('Buzz');
+                    break
+                default :
+                    console.log('' + count);
+            }
+        }
+    }
+    fizzBuzz();
+}())
