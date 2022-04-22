001439e3e34f3603d6b8fc9cf65b0aa60d4c131d
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-22 16:03:44 -0500

    simplified and commented narcissist number function

--- js/general_coding_challenges.js
+++ js/general_coding_challenges.js
@@ -9,19 +9,19 @@
     function narcissistNumber(maxCount) {
         maxCount = parseInt(prompt('How many narcissistic integers would you like me to introduce to you today?', '25'))
         let count = 0; //sets count as zero, as we haven't found any yet.
-        let n = 10; // sets n to 10, as the first multidigit number
+        let n = 100; // sets n to 100, as the starting point as there are no 2 digit narcissistic numbers
         do {
             let digitArray = Array.from(String(n),Number); // takes n, converts it to a string, and the breaks that string into an array of the digits, the converts them back to integers
             m = digitArray.length; // sets the power for the Math.pow function below to the length of the array just created
             let y = 0; // establishes local variable to add results of the for loop below too
             for (let digit of digitArray) {
-                y = y + Math.pow(digit,m); // for each digit in the array raises the individual digit to the power of the amount of numbers that compose the number and adds it to the local variable y
+                y += Math.pow(digit,m); // for each digit in the array raises the individual digit to the power of the amount of numbers that compose the number and adds it to the local variable y
                 if (n === y) { //checks if the local variable y is equivalent to the original number
-                    console.log(n + ' is a narcissistic number.');
-                    count ++;
+                    console.log(n + ' is a narcissistic number.'); //logs the number in the console if it is a narcissistic number
+                    count ++;// adds to the count
                 }
             }
-            n += 1;
-        } while (count <= maxCount);
+            n += 1;// increases the number by one to start the check over
+        } while (count <= maxCount);// continues doing the above code until the desired number is reached
     }narcissistNumber();
 })()
