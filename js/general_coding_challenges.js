99e83accd5affe11ebbd156607e43e55d7828809
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-22 17:01:10 -0500

    cleaned up narcissit numbers a little bit

--- js/general_coding_challenges.js
+++ js/general_coding_challenges.js
@@ -8,14 +8,13 @@
 //     Objective: Write a script to generate and output the first 25 narcissistic integers.
     function narcissistNumber(maxCount) {
         maxCount = parseInt(prompt('How many narcissistic integers would you like me to introduce to you today?', '25'))
-        let count = 1; //sets count as zero, as we haven't found any yet.
+        let count = 1; //sets count to 1
         let n = 100; // sets n to 100, as the starting point as there are no 2 digit narcissistic numbers
         do {
             let digitArray = Array.from(String(n),Number); // takes n, converts it to a string, and the breaks that string into an array of the digits, the converts them back to integers
-            m = digitArray.length; // sets the power for the Math.pow function below to the length of the array just created
             let y = 0; // establishes local variable to add results of the for loop below too
             for (let digit of digitArray) {
-                y += Math.pow(digit,m); // for each digit in the array raises the individual digit to the power of the amount of numbers that compose the number and adds it to the local variable y
+                y += Math.pow(digit,digitArray.length); // for each digit in the array raises the individual digit to the power of the amount of numbers that compose the number and adds it to the local variable y
                 }
             if (n === y) { //checks if the local variable y is equivalent to the original number
                 console.log(n + ' is a narcissistic number.'); //logs the number in the console if it is a narcissistic number
