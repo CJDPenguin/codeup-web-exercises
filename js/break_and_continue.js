ce22ea3669f675b3a4c890547a5f52d0431aadd5
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-22 14:00:31 -0500

    arrays exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,16 +1,16 @@
 (function () {
     "use strict";
-    // function skipToMyLoo (input) {
-    //     do {
-    //         input = parseInt(prompt('Please enter an odd number between 1 & 50'));
-    //     } while (input % 2 === 0 || input <= 0 || input >50);
-    //     for (let count = 1; count < 50; count +=2) {
-    //         if (count !== input) {
-    //             console.log(count)
-    //         }
-    //     }
-    // } skipToMyLoo()
-    function skipToMyLooBrokenContinued(input) {
+    function skipToMyLoo (input) { //This was my first idea but didn't make use of the break & continue commands
+        do {
+            input = parseInt(prompt('Please enter an odd number between 1 & 50'));
+        } while (input % 2 === 0 || input <= 0 || input >50);
+        for (let count = 1; count < 50; count +=2) {
+            if (count !== input) {
+                console.log(count)
+            }
+        }
+    } skipToMyLoo()
+    function skipToMyLooBrokenContinued(input) {//using break & continue commands
         do {
             input = parseInt(prompt('Please enter an odd number between 1 & 50'));
             if (input % 2 !== 0 && input > 0 && input <= 50) {
--- js/while.js
+++ js/while.js
@@ -15,11 +15,33 @@
             let yummy = Math.floor(Math.random() * 5) + 1
             if (yummy > inventory) {
                 console.log('Drat, the customer wants ' + yummy + ' cones but I only have ' + inventory + " cones left, I can't make that sale.");
-                break
+                continue
             }
             console.log('I sold ' + yummy + ' cones, I have ' + (inventory - yummy) + ' cones left, better keep selling.');
             inventory -= yummy;
         } while (inventory > 0);
         console.log('Time to blow this popsicle stand!');
     } letsGetSomeIceCream();
+//practice parameterizing, & refactoring function
+    function letsSellTillWeDrop(){
+        let whatWeSelling = prompt("What are we selling today?", "Things")
+        if (whatWeSelling === undefined) {
+            whatWeSelling = 'Thingamabobs'
+        }
+        let howMuchWeGot = parseInt(prompt('How many of them do we have?'));
+        if (howMuchWeGot === undefined) {
+            howMuchWeGot = (Math.random() * 50) + 50
+        }
+        console.log('I have ' + howMuchWeGot + " " + whatWeSelling + ". Let's get to selling");
+        do {
+            let sale = Math.floor(Math.random() * 5) + 1
+            if (sale > howMuchWeGot) {
+                console.log('Drat, the customer wants ' + sale + ' but I only have ' + howMuchWeGot + " left, I can't make that sale.");
+                continue
+            }
+            howMuchWeGot -= sale
+            console.log('I sold ' + sale + ', I have ' + (howMuchWeGot) + ' left, better keep selling.');
+        } while (howMuchWeGot > 0);
+        console.log('Time to blow this popsicle stand!');
+    } letsSellTillWeDrop();
 })();
