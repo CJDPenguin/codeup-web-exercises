bc2a3f53ddd4d1da23ad5567696d1df7b08fb9ee
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-27 13:13:43 -0500

    finished stopwatch

--- js/objects_practice.js
+++ js/objects_practice.js
@@ -9,14 +9,14 @@
     cars.push(car);
     }
 
-    // while(confirm('Would you like to enter a car?')) {
-    //     makeCar();
-    // }
-    // console.log(cars)
+    while(confirm('Would you like to enter a car?')) {
+        makeCar();
+    }
+    console.log(cars)
 // Ex 2
     const salesReport = {
         title: 'Monthly Sales Report',
-        date: Date.UTC(2015,3,17),
+        date: new Date(2015,3,17),
         office: 'Codeup',
         employees: [{
             employeeNumber : 1,
@@ -77,8 +77,10 @@
     }
     console.log(salesReport);
     //Ex 3
-    salesReport.getEmployeeCount = salesReport.employees.length //returns the number of employees in the sales report
-    console.log(salesReport.getEmployeeCount);
+    salesReport.getEmployeeCount = function () {
+       return salesReport.employees.length;
+    } //returns the number of employees in the sales report
+    console.log('There were ' + salesReport.getEmployeeCount() + ' employees active on ' + salesReport.date.toDateString());
     // gets the total units sold by all employees
     salesReport.getTotalNumberOfSales = function () {
         let total = 0;
@@ -86,21 +88,43 @@
          total += employee.salesUnits;
      }
     return total}
-    console.log(salesReport.getTotalNumberOfSales());
+    console.log('There were ' + salesReport.getTotalNumberOfSales() + ' units sold on ' + salesReport.date.toDateString());
     //gets average units sold by employee
     salesReport.getAverageSalesPerEmployee = function () {
-        return salesReport.getTotalNumberOfSales() / salesReport.getEmployeeCount
+        return this.getTotalNumberOfSales() / this.getEmployeeCount();
     }
-    console.log(salesReport.getAverageSalesPerEmployee());
+    console.log('The average number of sales were ' + salesReport.getAverageSalesPerEmployee()+ ' per employee.');
     //Stopwatch object
     const stopWatch = {
         intervalId: 1,
-        count: 0
+        count: 0,
+        start(){
+            this.intervalId = setInterval(() => {
+                console.log(this.count);
+                this.count++
+            }, 1000)
+        },
+        pause(){
+            clearInterval(this.intervalId);
+            console.log(this.count)
+        },
+        reset(){
+            console.log(this.count);
+            this.count = 0;
+        },
+        stop(){
+            clearInterval(this.intervalId);
+            console.log(this.count);
+            this.count = 0;
+        }
     }
 
-    stopWatch.start = setInterval(function () {
-        console.log(stopWatch.count);
-        stopWatch.count++
-    },1000)
-
+    stopWatch.start();
+    setTimeout(() => stopWatch.pause(), 10000);
+    setTimeout(() => stopWatch.start(), 15000);
+    setTimeout(() => stopWatch.reset(), 18000);
+    setTimeout(() => stopWatch.pause(), 20000);
+    setTimeout(() => stopWatch.stop(), 25000);
+    setTimeout(() => stopWatch.start(), 30000);
+    setTimeout(() => stopWatch.pause(), 40000);
 })()
