4a9df4563d6a94f0af6fffa44a3c271575f834c9
Chris Davis <christopher.j.davis03@gmail.com>
2022-04-28 09:16:02 -0500

    warmups

--- js/warmups.js
+++ js/warmups.js
@@ -1,7 +1,7 @@
 (function() {
     "use strict"
     console.log("Yay! I completed the warmup!")
-
+//Fizzbuzz
     function fizzBuzz() {
         for (let count = 1; count <= 100; count++) {
             switch (true) {
@@ -20,7 +20,7 @@
         }
     }
     fizzBuzz();
-
+//Abbreviated
     function abbreviate(array) {
        let abbreviated = "";
        for (let letter of array) {
@@ -30,4 +30,11 @@
     }
     const array1 = ["dog", "cat", "frog", "bat"]
     console.log(abbreviate(array1))
+
+    //Write a function that takes in a string and returns that string in alphabetical order.
+    function alphabetize(string){
+       return string.split('').sort().join('');
+    }
+
+    console.log(alphabetize("codeup"));
 }())
