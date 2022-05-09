b8816e3de97f704e8572f271fb41de129ea406b1
Chris Davis <christopher.j.davis03@gmail.com>
2022-05-09 13:37:21 -0500

    tblock resume exercise and warmup

--- js/warmups.js
+++ js/warmups.js
@@ -48,7 +48,7 @@
     //write a function, returnProductDetails, that takes in a product object and returns some of the details.
 let productArray = {
     product1 : {
-        name: 'Hammar',
+        name: 'Hammer',
         priceInCents: 400,
         description: 'It is a a hammar.',
         inventory: 25034
@@ -85,4 +85,50 @@ let productArray = {
     //     return essentialDetails;
     // }
     // returnAllProductEssentialDetails(productArray);
+
+    //Write a function that will accept an array of objects and return the tallest object from the array.
+    const hamsters = [
+        {
+            name: "Hamtaro",
+            heightInMM: 86,
+            fur: ['orange', 'white'],
+            gender: "male",
+            dateOfBirth: "August 6"
+        } , {
+            name: "Bijou",
+            heightInMM: 75,
+            fur: ['white'],
+            gender: "female",
+            dateOfBirth: "July 10"
+        } , {
+            name: "Oxnard",
+            heightInMM: 100,
+            fur: ['grey', 'white'],
+            gender: "male",
+            dateOfBirth: "May 3"
+        } , {
+            name: "Boss",
+            heightInMM: 120,
+            fur: ['brown', 'white'],
+            gender: "male",
+            dateOfBirth: "Spetember 21"
+        } , {
+            name: "Snoozer",
+            heightInMM: 85,
+            fur: ['brown', 'white', "pink"],
+            gender: "male",
+            dateOfBirth: "January 14"
+        }
+    ];
+
+    function tallestObject(array){
+        let tallest = {heightInMM: 0};
+        for (let hamster of array) {
+            if (hamster.heightInMM > tallest.heightInMM){
+                tallest = hamster;
+            }
+        }
+        return tallest;
+    }
+    console.log(tallestObject(hamsters));
 }())
