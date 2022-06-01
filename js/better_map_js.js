4c3018942de13d16139fe42d6547fac7c77d38b3
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-01 16:40:45 -0500

    weather proj

--- js/better_map_js.js
+++ js/better_map_js.js
@@ -1,122 +1,108 @@
 //sets up date and time in legible form
-let dateTime = function (dateData) {
-    let date = new Date(dateData * 1000)
-    let d = function () {
-        if (date.getDate() < 10) {
-            return '0' + date.getDate()
-        }
-        return date.getDate()
-    }
-    let m = function () {
-        switch (date.getMonth()) {
-            case 0:
-                return ("JAN");
-            case 1:
-                return ("FEB");
-            case 2:
-                return ("MAR");
-            case 3:
-                return ("APR");
-            case 4:
-                return ("MAY");
-            case 5:
-                return ("JUN");
-            case 6:
-                return ("JUL");
-            case 7:
-                return ("AUG");
-            case 8:
-                return ("SEP");
-            case 9:
-                return ("OCT");
-            case 10:
-                return ("NOV");
-            case 11:
-                return ("DEC");
-        }
-    }
-    let y = date.getFullYear();
-    let time = date.toTimeString().substring(0, 5);
-    return d() + ' ' + m() + ' ' + y + "<br>" + time;
-}
-
-function wxIcon(data) {
-    let wxID = data.slice(0, 2);
-    switch (wxID) {
-        case '01': //clear sky
-            wxID = '<ion-icon name="sunny-sharp"></ion-icon>';
-            return wxID;
-        case '02': //few clouds
-            wxID = '<ion-icon name="sunny-sharp"></ion-icon>';
-            return wxID;
-        case '03': //scattered clouds
-            wxID = '<ion-icon name="partly-sunny-sharp"></ion-icon>';
-            return wxID;
-        case '04': //broken clouds
-            wxID = '<ion-icon name="cloud-sharp"></ion-icon>';
-            return wxID;
-        case '09': //shower rain
-            wxID = '<ion-icon name="rainy-sharp"></ion-icon>';
-            return wxID;
-        case '10': //rain
-            wxID = '<ion-icon name="rainy-sharp"></ion-icon>';
-            return wxID;
-        case '11': //thunderstorm
-            wxID = '<ion-icon name="thunderstorm-sharp"></ion-icon>';
-            return wxID;
-        case '13': //snow
-            wxID = '<ion-icon name="snow-sharp"></ion-icon>';
-            return wxID;
-        case '50': //mist
-            wxID = '<ion-icon name="water-sharp"></ion-icon>';
-            return wxID;
-        default:
-            wxID = '<ion-icon name="cafe-sharp"></ion-icon> Insert Coffee to continue';
-            return wxID;
-    }
-}
-
-function forecastHTML(date) {
-    let html = '';
-    html += '<div>' + dateTime(date.dt).slice(0, 6) + '</div>';
-    html += '<div>' + wxIcon(date.weather[0].icon) + '</div>';
-    html += '<div>' + date.temp.max + '</div>';
-    html += '<div>' + date.temp.min + '</div>';
-    html += '<div>' + Math.round(date.pop * 100) + '%</div>';
-    return html;
-}
-
-function alertHTML(alert) {
-    let html = '';
-    html += '<div>From: ' + alert.sender_name + '</div>'
-    html += '<div>Event: ' + alert.event + '</div>'
-    html += '<div>Start: ' + dateTime(alert.start) + '</div>'
-    html += '<div>End: ' + dateTime(alert.end) + '</div>'
-    html += '<div>Details: ' + alert.description + '</div>'
-    return html;
-}
-
-mapboxgl.accessToken = MAPBOX_KEY;
 $(document).ready(function () {
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/cjdpenguin/cl3p0cdm4004n14myrccdh02i', // style URL
-        center: [-98.49, 29.42], // starting position [lng, lat]
-        zoom: 5, // starting zoom
-        attributionControl: true
-    });
 
-    const marker = new mapboxgl.Marker({color: 'black', draggable: true})
-        .setLngLat([-98.49, 29.42])
-        .addTo(map);
+    let dateTime = function (dateData) {
+        let date = new Date(dateData * 1000)
+        let d = function () {
+            if (date.getDate() < 10) {
+                return '0' + date.getDate()
+            }
+            return date.getDate()
+        }
+        let m = function () {
+            switch (date.getMonth()) {
+                case 0:
+                    return ("JAN");
+                case 1:
+                    return ("FEB");
+                case 2:
+                    return ("MAR");
+                case 3:
+                    return ("APR");
+                case 4:
+                    return ("MAY");
+                case 5:
+                    return ("JUN");
+                case 6:
+                    return ("JUL");
+                case 7:
+                    return ("AUG");
+                case 8:
+                    return ("SEP");
+                case 9:
+                    return ("OCT");
+                case 10:
+                    return ("NOV");
+                case 11:
+                    return ("DEC");
+            }
+        }
+        let y = date.getFullYear();
+        let time = date.toTimeString().substring(0, 5);
+        return d() + ' ' + m() + ' ' + y + "<br>" + time;
+    }
 
-    let pos = marker.getLngLat();
+    function wxIcon(data) {
+        let wxID = data.slice(0, 2);
+        switch (wxID) {
+            case '01': //clear sky
+                wxID = '<ion-icon name="sunny-sharp"></ion-icon>';
+                return wxID;
+            case '02': //few clouds
+                wxID = '<ion-icon name="sunny-sharp"></ion-icon>';
+                return wxID;
+            case '03': //scattered clouds
+                wxID = '<ion-icon name="partly-sunny-sharp"></ion-icon>';
+                return wxID;
+            case '04': //broken clouds
+                wxID = '<ion-icon name="cloud-sharp"></ion-icon>';
+                return wxID;
+            case '09': //shower rain
+                wxID = '<ion-icon name="rainy-sharp"></ion-icon>';
+                return wxID;
+            case '10': //rain
+                wxID = '<ion-icon name="rainy-sharp"></ion-icon>';
+                return wxID;
+            case '11': //thunderstorm
+                wxID = '<ion-icon name="thunderstorm-sharp"></ion-icon>';
+                return wxID;
+            case '13': //snow
+                wxID = '<ion-icon name="snow-sharp"></ion-icon>';
+                return wxID;
+            case '50': //mist
+                wxID = '<ion-icon name="water-sharp"></ion-icon>';
+                return wxID;
+            default:
+                wxID = '<ion-icon name="cafe-sharp"></ion-icon> Insert Coffee to continue';
+                return wxID;
+        }
+    }
 
+    function forecastHTML(date) {
+        let html = '';
+        html += '<div>' + dateTime(date.dt).slice(0, 6) + '</div>';
+        html += '<div>' + wxIcon(date.weather[0].icon) + '</div>';
+        html += '<div>' + date.temp.max + '</div>';
+        html += '<div>' + date.temp.min + '</div>';
+        html += '<div>' + Math.round(date.pop * 100) + '%</div>';
+        return html;
+    }
+
+    function alertHTML(alert) {
+        let html = '';
+        html += '<div>From: ' + alert.sender_name + '</div>'
+        html += '<div>Event: ' + alert.event + '</div>'
+        html += '<div>Start: ' + dateTime(alert.start) + '</div>'
+        html += '<div>End: ' + dateTime(alert.end) + '</div>'
+        html += '<div>Details: ' + alert.description + '</div>'
+        return html;
+    }
+
+    mapboxgl.accessToken = MAPBOX_KEY;
 
     function call(location) {
 
         $.get("https://api.openweathermap.org/data/2.5/onecall?lat=" + location.lat + "&lon=" + location.lng + "&units=imperial&appid=" + OPEN_WEATHER_KEY).done(function (data) {
-            console.log(data)
             reverseGeocode(location, MAPBOX_KEY).then((place) => {
                 let where = place.split(",")
                 $('#city').val(where[0] + "," + where[2]);
@@ -132,11 +118,24 @@ $(document).ready(function () {
             $('#dayThree').empty().append(forecastHTML(data.daily[3]));
             $('#dayFour').empty().append(forecastHTML(data.daily[4]));
             $('#dayFive').empty().append(forecastHTML(data.daily[5]));
-            data.alerts.forEach(alert => {
-                $('.alert').append(alertHTML(alert));
-            })
         })
+
     }
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/cjdpenguin/cl3p0cdm4004n14myrccdh02i', // style URL
+        center: [-98.49, 29.42], // starting position [lng, lat]
+        zoom: 5, // starting zoom
+        attributionControl: true
+    });
+
+    const marker = new mapboxgl.Marker({color: 'black', draggable: true})
+        .setLngLat([-98.49, 29.42])
+        .addTo(map);
+
+    let pos = marker.getLngLat();
+
 
     call(pos);
 
